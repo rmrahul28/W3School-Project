8b6de70bdfc73519871fb0b3c733bbfa589dccf4
Rahul28 <126143626+rmrahul28@users.noreply.github.com>
2023-05-17 09:28:05 +0530

    Dropdown on Hover

---
+++ Dropdown on Hover/index.js
@@ -0,0 +1,7 @@
+let main = document.getElementById("main");
+main.addEventListener("mouseover", function () {
+  main.style.height = "auto";
+});
+main.addEventListener("mouseout", function () {
+  main.style.height = "38px";
+});
