c53e1749af515f6d0b81685a95c3b4f99d1cc7b3
Rahul28 <126143626+rmrahul28@users.noreply.github.com>
2023-05-17 23:14:32 +0530

    Sidenav Push w/opacity

---
+++ 16.4 Sidenav Push opacity/index.js
@@ -0,0 +1,15 @@
+let open = document.getElementById("open");
+let close = document.getElementById("closebtn")
+open.addEventListener("click", openNav);
+close.addEventListener("click", closeNav);
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+  }
